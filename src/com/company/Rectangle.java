e3892f5bfad34e509fbd9559b7f335a94ab634be
retion111 <retion428@gmail.com>
2021-11-15 16:21:33 +0200

    add

--- src/com/company/Rectangle.java
+++ src/com/company/Rectangle.java
@@ -1,10 +1,22 @@
 package com.company;
 
+
+import java.util.Scanner;
+
 public class Rectangle {
-
-
     public static void main(String[] args) {
 
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите длину прямоугольника: ");
+        double side1 = scanner.nextDouble();
+        System.out.println("Введите ширину прямоугольника: ");
+        double side2 = scanner.nextDouble();
+        double areaCalculator = (side1 * side2);
+        double perimeterCalculator = (side1 + side2) * 2;
+
+
+        System.out.println("Площадь прямоугольника: " + areaCalculator);
+        System.out.println("Периметра прямоугольника: " + perimeterCalculator);
     }
 }
